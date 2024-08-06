9cd7f60dc8aa40a8c9165a619198cf82669acfdd
David SG <santome.galvan.david@gmail.com>
2024-08-06 22:31:47 +0200

    [Feature] Window and structure styling

--- main.js
+++ main.js
@@ -1,26 +1,34 @@
-const { app, BrowserWindow } = require("electron/main");
+const { app, BrowserWindow } = require("electron");
 
-const createWindow = () => {
-	const win = new BrowserWindow({
-		width: 800,
-		height: 600,
+function createWindow() {
+	const mainWindow = new BrowserWindow({
+		width: 1000,
+		height: 700,
+		webPreferences: {
+			nodeIntegration: true,
+			contextIsolation: false,
+		},
+		titleBarStyle: "hidden",
+		titleBarOverlay: {
+			color: "#1f1f1f",
+			symbolColor: "#fff",
+			height: 60,
+		},
 	});
+	// mainWindow.webContents.openDevTools();
+	mainWindow.loadFile("index.html");
+}
 
-	win.loadFile("index.html");
-};
-
-app.whenReady().then(() => {
-	createWindow();
-
-	app.on("activate", () => {
-		if (BrowserWindow.getAllWindows().length === 0) {
-			createWindow();
-		}
-	});
-});
+app.on("ready", createWindow);
 
 app.on("window-all-closed", () => {
 	if (process.platform !== "darwin") {
 		app.quit();
+	}
+});
+
+app.on("activate", () => {
+	if (BrowserWindow.getAllWindows().length === 0) {
+		createWindow();
 	}
 });
